f16d37cb44de042e15ade6d33aea255785680ee1
NirlepTamboli <nirleptamboli@gmail.com>
2025-06-12 20:20:06 -0400

    created forgot password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Login';
-import Register from './Register';
-import ForgotPassword from './ForgotPassword';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   const [count, setCount] = useState(0)
